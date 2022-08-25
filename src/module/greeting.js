903a199061fe480c72d031d17a59f4864e22bdf6
vantamin <vantablack.min@gmail.com>
2022-08-25 15:28:27 +0900

    test: greeting class

--- src/module/greeting.js
+++ src/module/greeting.js
@@ -1,3 +1,9 @@
 export function greeting(name) {
   console.log('hello ' + name);
 }
+
+export class Greeting {
+  sayHello(name) {
+    console.log('hello ' + name);
+  }
+}
--- src/module/main.js
+++ src/module/main.js
@@ -1,3 +1,7 @@
 import { greeting } from 'greeting.js';
+import { Greeting } from 'greeting.js';
 
 greeting('world');
+
+let g = new Greeting();
+g.sayHello('world');
